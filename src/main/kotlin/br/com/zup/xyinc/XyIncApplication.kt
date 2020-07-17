4910e09b1feaba077cf31ba43996b5451c4576b2
Icaro Gabriel Marques Afonso <icaromarques.ti@gmail.com>
2020-07-17 18:09:24 -0300

    Initial commit

---
+++ src/main/kotlin/br/com/zup/xyinc/XyIncApplication.kt
@@ -0,0 +1,11 @@
+package br.com.zup.xyinc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class XyIncApplication
+
+fun main(args: Array<String>) {
+	runApplication<XyIncApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/zup/xyinc/XyIncApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.zup.xyinc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class XyIncApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
