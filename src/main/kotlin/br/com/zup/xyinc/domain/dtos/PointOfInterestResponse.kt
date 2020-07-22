c7c939e1e6cff3ad62cda375dbbc46a23c03d511
Icaro Gabriel Marques Afonso <icaroafonsozup>
2020-07-22 12:20:51 -0300

    Alterados profiles, Swagger, e preenchido README.md

--- src/main/kotlin/br/com/zup/xyinc/domain/dtos/PointOfInterestResponse.kt
+++ src/main/kotlin/br/com/zup/xyinc/domain/dtos/PointOfInterestResponse.kt
@@ -2,13 +2,18 @@ package br.com.zup.xyinc.domain.dtos
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
+import io.swagger.annotations.ApiModelProperty
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class PointOfInterestResponse(
         @get:JsonProperty(value = "xCoordinate")
+        @get:ApiModelProperty(value = "Coordenada X")
         val xCoordinate: Int,
         @get:JsonProperty(value = "yCoordinate")
+        @get:ApiModelProperty(value = "Coordenada Y")
         val yCoordinate: Int,
+        @get:ApiModelProperty(value = "Nome do ponto de interesse")
         val name: String,
+        @get:ApiModelProperty(value = "Id do ponto de interesse")
         val id: String? = null
 )
